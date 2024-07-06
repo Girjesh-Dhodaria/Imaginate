2c77ce2072797ec2e086dd349228598938452f91
Girejsh-Dhodaria <girjeshdhodaria@gmail.com>
2024-07-06 22:22:53 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+      {/* <p>Home</p>
+      
+      <UserButton /> */}
+    </div>
+  )
+}
+
+export default Home
+
+
